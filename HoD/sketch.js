4eb18e090d4d8cb93df9b94079788533b4e7e891
Jeff Guess <lists@guess.fr>
2021-01-09 12:12:48 +0100

    added json

--- HoD/sketch.js
+++ HoD/sketch.js
@@ -1,11 +1,9 @@
-
-var material = ["SAND", "DUST", "LEAVES", "PAPER", "TIN", "ROOTS", "BRICK", "STONE", "DISCARDED CLOTHING", "GLASS", "STEEL", "PLASTIC", "MUD", "BROKEN DISHES", "WOOD", "STRAW", "WEEDS"]
-var light_source = ["CANDLES", "ALL AVAILABLE LIGHTING", "ELECTRICITY", "NATURAL LIGHT"]
-var inhabitants = ["PEOPLE WHO SLEEP VERY LITTLE", "VEGETARIANS", "HORSES AND BIRDS", "PEOPLE SPEAKING MANY LANGUAGES WEARING LITTLE OR NO CLOTHING", "ALL RACES OF MEN REPRESENTED WEARING PREDOMINANTLY RED CLOTHING", "CHILDREN AND OLD PEOPLE", "VARIOUS BIRDS AND FISH", "LOVERS", "PEOPLE WHO ENJOY EATING TOGETHER", "PEOPLE WHO EAT A GREAT DEAL", "COLLECTORS OF ALL TYPES", "FRIENDS AND ENEMIES", "PEOPLE WHO SLEEP ALMOST ALL THE TIME", "VERY TALL PEOPLE", "AMERICAN INDIANS", "LITTLE BOYS", "PEOPLE FROM MANY WALKS OF LIFE", "NEGROS WEARING ALL COLORS", "FRIENDS", "FRENCH AND GERMAN SPEAKING PEOPLE", "FISHERMEN AND FAMILIES", "PEOPLE WHO LOVE TO READ"]
-var loca = ["IN A GREEN, MOSSY TERRAIN", "IN AN OVERPOPULATED AREA", "BY THE SEA", "BY AN ABANDONED LAKE", "IN A DESERTED FACTORY", "IN DENSE WOODS", "IN JAPAN", "AMONG SMALL HILLS", "IN SOUTHERN FRANCE", "AMONG HIGH MOUNTAINS", "ON AN ISLAND", "IN A COLD, WINDY CLIMATE", "IN A PLACE WITH BOTH HEAVY RAIN AND BRIGHT SUN", "IN A DESERTED AIRPORT", "IN A HOT CLIMATE", "INSIDE A MOUNTAIN", "ON THE SEA", "IN MICHIGAN", "IN HEAVY JUNGLE UNDERGROWTH", "BY A RIVER", "AMONG OTHER HOUSES", "IN A DESERTED CHURCH", "IN A METROPOLIS", "UNDERWATER"]
-
 var tout = ""
-var button;
+var button
+var data
+function preload() {
+	data = loadJSON("data.json");
+}
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -20,13 +18,15 @@ function changeBG() {
 
     tout = ""
     
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 1000; i++) {
         
-        tout += "A HOUSE OF " + choice(material) + "\n"
-        tout += "      " + choice(loca) + "\n"
-        tout += "            USING " + choice(light_source) + "\n"
-        tout += "                  INHABITED BY " + choice(inhabitants) + "\n\n\n"
+        tout += "A HOUSE OF " + choice(data.material) + "\n"
+        tout += "      " + choice(data.loca) + "\n"
+        tout += "            USING " + choice(data.light_source) + "\n"
+        tout += "                  INHABITED BY " + choice(data.inhabitants) + "\n\n\n"
     }
+
+    // print(tout)
 
     var blob = new Blob([tout],
         { type: "text/plain;charset=utf-8" });
